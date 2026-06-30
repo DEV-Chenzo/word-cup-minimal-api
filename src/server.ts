0f6bb7c430b131770ca14a47d3ea539a92b21cc9
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-06-30 15:39:13 -0300

    create logic and services functions

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,32 @@
-import fastify from 'fastify'
-import {getWordCupData} from './services/services.ts'
+import fastify from "fastify";
+import {
+  getWordCupData,
+  getTeamsData,
+  getTeamsByFieldHandler,
+} from "./services/services.ts";
 const server = fastify({
-  logger: true
-})
+  logger: true,
+});
 
-server.get('/api/wordcup', await getWordCupData)
+server.get("/wordcup", await getWordCupData);
+server.get("/teams", await getTeamsData);
+server.get("/teams/:id", await getTeamsByFieldHandler);
+
+const start = async () => {
+  try {
+    const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+    // 2. Roda o servidor
+    await server.listen({
+      port: port,
+      host: "0.0.0.0", // CRÍTICO para o deploy funcionar!
+    });
+
+    console.log(`Servidor rodando na porta ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
--- src/services/services.ts
+++ src/services/services.ts
@@ -1,25 +1,55 @@
-import type { FastifyReply, FastifyRequest } from "fastify"
+import type { FastifyReply, FastifyRequest } from "fastify";
 // @ts-ignore: module has no declaration file
-import Teams from "../data/teams"
+import { Teams } from "../data/teams";
 // @ts-ignore: module has no declaration file
-import WordCupWinners from "../data/word-cup-winners"
-import type { MyTeams ,TeamParams } from "./services.type.ts"
-export async function getWordCupData(request: FastifyRequest , response: FastifyReply ) {
-  response.type("application/json").code(200)
-  return WordCupWinners
+import { WorldCupWinners } from "../data/word-cup-winners";
+import type { MyTeams, TeamParams } from "./services.type.ts";
+
+export async function getWordCupData(
+  request: FastifyRequest,
+  response: FastifyReply,
+) {
+  response.type("application/json").code(200);
+  return WorldCupWinners;
 }
 
-export async function getTeamByIdHandler(
-  request: FastifyRequest<{ Params: TeamParams }>,
-  reply: FastifyReply
+export async function getTeamsData(
+  request: FastifyRequest,
+  response: FastifyReply,
 ) {
-  const { id } = request.params; 
-  const team = Teams.find((t: MyTeams) => t.fifa_code === id.toUpperCase());
-  if (!team) {
-    reply.type("application/json").code(404);
-    return { message: "Seleção não encontrada" };
-  }
+  response.type("application/json").code(200);
+  return Teams;
+}
 
-  reply.type("application/json").code(200);
-  return { team };
+export async function getTeamsByFieldHandler(
+  request: FastifyRequest<{ Params: TeamParams }>,
+  reply: FastifyReply,
+) {
+  const { id } = request.params;
+
+  // 1. Verificamos se o que foi digitado é um número (ex: "5")
+  const isNumber = /^\d+$/.test(id);
+
+  if (isNumber) {
+    const titlesCount = parseInt(id, 10);
+    // Filtra todas as seleções que têm aquele número exato de títulos
+    const filteredTeams = Teams.filter(
+      (t: MyTeams) => t.world_cup_titles === titlesCount,
+    );
+
+    reply.type("application/json").code(200);
+    return { Teams: filteredTeams };
+  } else {
+    const team = Teams.find(
+      (t: MyTeams) => t.name.toLowerCase() === id.toLowerCase(),
+    );
+
+    if (!team) {
+      reply.type("application/json").code(404);
+      return { message: `Seleção '${id}' não encontrada.` };
+    }
+
+    reply.type("application/json").code(200);
+    return { team };
+  }
 }
