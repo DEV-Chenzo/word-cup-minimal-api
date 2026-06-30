5b5399757cff69f1d9ef4ecb5ac9456322a7c48b
DEV-Chenzo <vicenzowebdeveloper@gmail.com>
2026-06-30 13:32:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# word-cup-minimal-api
+Uma API minima criada para estudo
---
+++ src/data/word-cup-winners.js
@@ -0,0 +1,157 @@
+
+export const world_cup_winners = [
+    {
+      "year": 1930,
+      "host": "Uruguai",
+      "winner": "Uruguai",
+      "runner_up": "Argentina",
+      "score": "4-2"
+    },
+    {
+      "year": 1934,
+      "host": "Itália",
+      "winner": "Itália",
+      "runner_up": "Tchecoslováquia",
+      "score": "2-1 (prorrogação)"
+    },
+    {
+      "year": 1938,
+      "host": "França",
+      "winner": "Itália",
+      "runner_up": "Hungria",
+      "score": "4-2"
+    },
+    {
+      "year": 1950,
+      "host": "Brasil",
+      "winner": "Uruguai",
+      "runner_up": "Brasil",
+      "score": "2-1 (fase final)"
+    },
+    {
+      "year": 1954,
+      "host": "Suíça",
+      "winner": "Alemanha Ocidental",
+      "runner_up": "Hungria",
+      "score": "3-2"
+    },
+    {
+      "year": 1958,
+      "host": "Suécia",
+      "winner": "Brasil",
+      "runner_up": "Suécia",
+      "score": "5-2"
+    },
+    {
+      "year": 1962,
+      "host": "Chile",
+      "winner": "Brasil",
+      "runner_up": "Tchecoslováquia",
+      "score": "3-1"
+    },
+    {
+      "year": 1966,
+      "host": "Inglaterra",
+      "winner": "Inglaterra",
+      "runner_up": "Alemanha Ocidental",
+      "score": "4-2 (prorrogação)"
+    },
+    {
+      "year": 1970,
+      "host": "México",
+      "winner": "Brasil",
+      "runner_up": "Itália",
+      "score": "4-1"
+    },
+    {
+      "year": 1974,
+      "host": "Alemanha Ocidental",
+      "winner": "Alemanha Ocidental",
+      "runner_up": "Países Baixos",
+      "score": "2-1"
+    },
+    {
+      "year": 1978,
+      "host": "Argentina",
+      "winner": "Argentina",
+      "runner_up": "Países Baixos",
+      "score": "3-1 (prorrogação)"
+    },
+    {
+      "year": 1982,
+      "host": "Espanha",
+      "winner": "Itália",
+      "runner_up": "Alemanha Ocidental",
+      "score": "3-1"
+    },
+    {
+      "year": 1986,
+      "host": "México",
+      "winner": "Argentina",
+      "runner_up": "Alemanha Ocidental",
+      "score": "3-2"
+    },
+    {
+      "year": 1990,
+      "host": "Itália",
+      "winner": "Alemanha Ocidental",
+      "runner_up": "Argentina",
+      "score": "1-0"
+    },
+    {
+      "year": 1994,
+      "host": "Estados Unidos",
+      "winner": "Brasil",
+      "runner_up": "Itália",
+      "score": "0-0 (3-2 pênaltis)"
+    },
+    {
+      "year": 1998,
+      "host": "França",
+      "winner": "França",
+      "runner_up": "Brasil",
+      "score": "3-0"
+    },
+    {
+      "year": 2002,
+      "host": "Coreia do Sul/Japão",
+      "winner": "Brasil",
+      "runner_up": "Alemanha",
+      "score": "2-0"
+    },
+    {
+      "year": 2006,
+      "host": "Alemanha",
+      "winner": "Itália",
+      "runner_up": "França",
+      "score": "1-1 (5-3 pênaltis)"
+    },
+    {
+      "year": 2010,
+      "host": "África do Sul",
+      "winner": "Espanha",
+      "runner_up": "Países Baixos",
+      "score": "1-0 (prorrogação)"
+    },
+    {
+      "year": 2014,
+      "host": "Brasil",
+      "winner": "Alemanha",
+      "runner_up": "Argentina",
+      "score": "1-0 (prorrogação)"
+    },
+    {
+      "year": 2018,
+      "host": "Rússia",
+      "winner": "França",
+      "runner_up": "Croácia",
+      "score": "4-2"
+    },
+    {
+      "year": 2022,
+      "host": "Catar",
+      "winner": "Argentina",
+      "runner_up": "França",
+      "score": "3-3 (4-2 pênaltis)"
+    }
+  ]
